de789f40ef555e542d4700dd38814c6471c72cb2
Anders Kalør <anders@kaloer.com>
2015-03-07 22:23:37 +0100

    Updated directory structure

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cp BB-ROASTER-01-00A0.dtbo /lib/firmware/
-cp BB-ROASTERPWM-01-00A0.dtbo /lib/firmware/
-cp BB-SPI0-01-00A0.dtbo /lib/firmware/
+cp dts/BB-ROASTER-01-00A0.dtbo /lib/firmware/
+cp dts/BB-ROASTERPWM-01-00A0.dtbo /lib/firmware/
+cp dts/BB-SPI0-01-00A0.dtbo /lib/firmware/
 echo BB-SPI0-01 > /sys/devices/bone_capemgr.9/slots
 echo BB-ROASTER-01 > /sys/devices/bone_capemgr.9/slots
 echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots
