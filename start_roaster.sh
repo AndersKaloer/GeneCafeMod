71945aade675912d0af74686ede37cc696879def
Anders Kalør <anders@kaloer.com>
2018-01-21 21:29:21 +0100

    Remove PWM check from start script
    This is no longer needed as we export it in setup.sh

--- start_roaster.sh
+++ start_roaster.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 ./setup.sh
-while [ ! -f /sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip0/pwm1/enable ]
-do
-  sleep 2
-done
+
 while [ ! -f "/sys/bus/iio/devices/iio:device0/in_voltage6_raw" ]
 do
   sleep 2
