1e03f5338cdaa3b8e3c6c2dd8832c23b8e7acd0c
Anders Kalør <anders@kaloer.com>
2018-01-21 11:13:46 +0100

    Update to kernel 4

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cp dts/BB-ROASTER-01-00A0.dtbo /lib/firmware/
 cp dts/BB-ROASTERPWM-01-00A0.dtbo /lib/firmware/
-cp dts/BB-SPI0-01-00A0.dtbo /lib/firmware/
-echo BB-SPI0-01 > /sys/devices/bone_capemgr.9/slots
-echo BB-ROASTER-01 > /sys/devices/bone_capemgr.9/slots
-echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots
-echo BB-ROASTERPWM-01 > /sys/devices/bone_capemgr.9/slots
+#cp dts/BB-SPI0-01-00A0.dtbo /lib/firmware/
+#echo BB-SPI0-01 > /sys/devices/bone_capemgr.9/slots
+#echo BB-ROASTER-01 > /sys/devices/bone_capemgr.9/slots
+#echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots
+#echo BB-ROASTERPWM-01 > /sys/devices/bone_capemgr.9/slots
+
+echo 1 > /sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip0/export
--- start_roaster.sh
+++ start_roaster.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 ./setup.sh
-while [ ! -f /sys/devices/ocp.3/roaster0_m150_pwm.13/run ]
+while [ ! -f /sys/devices/platform/ocp/48302000.epwmss/48302200.pwm/pwm/pwmchip0/pwm1/enable ]
+do
+  sleep 2
+done
+while [ ! -f "/sys/bus/iio/devices/iio:device0/in_voltage6_raw" ]
 do
   sleep 2
 done
 ./roaster &
-sh -c 'cd webserver && node index.jl'
+sh -c 'cd webserver && node index.js'
